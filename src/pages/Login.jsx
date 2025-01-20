8eef81a4749126dad594f01f703eb995497bfee2
lbrahimali <ibroemail1@gmail.com>
2025-01-21 01:04:12 +0300

    vercel.json

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -179,15 +179,15 @@ const LoginPage = () => {
 
               <div>
                 <label
-                  htmlFor="password"
+                  htmlFor="Password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Şifre
                 </label>
                 <input
-                  type="text"
+                  type="password"
                   name="Password"
-                  id="password"
+                  id="Password"
                   onChange={handleChange}
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
