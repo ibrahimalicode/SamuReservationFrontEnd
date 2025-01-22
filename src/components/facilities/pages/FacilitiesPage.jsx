895cc0bd241cd71a53ea275e1387e4d203851d87
lbrahimali <ibroemail1@gmail.com>
2025-01-23 01:10:32 +0300

    ui fix

--- src/components/facilities/pages/FacilitiesPage.jsx
+++ src/components/facilities/pages/FacilitiesPage.jsx
@@ -27,12 +27,12 @@ const FacilitiesPage = () => {
         )}
       </div>
 
-      <div className="flex gap-4 mb-3">
+      <div className="flex gap-4 mb-3 overflow-x-auto">
         {facilitiesData?.map((fac) => (
           <button
             key={fac.id}
             onClick={() => setTableData(fac)}
-            className={`text-sm px-5 py-2.5 ${
+            className={`text-sm px-5 py-2.5 whitespace-nowrap ${
               fac.id == tableData?.id
                 ? "text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 : "text-gray-900 focus:outline-none bg-white rounded-lg border-2 border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
